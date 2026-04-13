3908c674519fc38be822e90376b30a8591e53cac
andorthehood <hngrhorace@gmail.com>
2026-04-13 20:00:25 +0200

    Initial Commit

---
+++ curvelab/curvelabApp.swift
@@ -0,0 +1,17 @@
+//
+//  curvelabApp.swift
+//  curvelab
+//
+//  Created by Andor Polgar on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct curvelabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ curvelabTests/curvelabTests.swift
@@ -0,0 +1,16 @@
+//
+//  curvelabTests.swift
+//  curvelabTests
+//
+//  Created by Andor Polgar on 13/04/2026.
+//
+
+import Testing
+
+struct curvelabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
